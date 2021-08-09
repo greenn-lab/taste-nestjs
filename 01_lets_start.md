0e99bb6bfe2763cd595d55ea9232e5ad7a5caef1
green <greenn.lab@gmail.com>
2021-08-09 12:27:04 +0900

    update// singleton & service pattern

--- 01_lets_start.md
+++ 01_lets_start.md
@@ -53,6 +53,7 @@ app.get('/cats/:id', (req: express.Request, res: express.Response) => {
 ```
 
 ## 디렉토리 구조
+
 ### Router 분리
 
 ```ts 
@@ -65,6 +66,7 @@ router.get('/cats', (req, res) => {
   // ... implements
 })
 ```
+
 라우터를 분리해서 생성하고
 
 ```ts
@@ -75,6 +77,62 @@ import catsRoute from './cats/cats.route'
 
 app.use(catsRoute)
 ```
+
 `app.ts` 에서 모두 취합하는 형태로.
 
+## Singleton, Service Pattern
 
+### class 로 instance 를 생성한 singleton
+
+```ts
+// app.ts
+class Server {
+  public app: express.Application
+
+  constructor() {
+    const app: express.Application = express()
+    this.app = app
+  }
+
+  // omitted...
+
+  public listen() {
+    this.setMiddleware()
+    this.app.listen(8000, () => {
+      console.log('server is on...')
+    })
+  }
+}
+
+function init() {
+  const server = new Server()
+  server.listen()
+}
+
+init()
+```
+
+### service
+
+```ts
+// cats.route.ts
+import {
+  createCat,
+  deleteCat,
+  readAllcat,
+  readCat,
+  updateCat,
+  updatePartialCat,
+} from './cats.service'
+
+const router = Router()
+
+router.get('/cats', readAllcat)
+router.get('/cats/:id', readCat)
+router.post('/cats', createCat)
+router.put('/cats/:id', updateCat)
+router.patch('/cats/:id', updatePartialCat)
+router.delete('/cats/:id', deleteCat)
+```
+
+`route` 에서 로직을 `cats.service.ts` 로 분리
--- 01_lets_start/src/app.ts
+++ 01_lets_start/src/app.ts
@@ -1,24 +1,49 @@
-import {default as express, Express, NextFunction, Request, Response} from 'express';
+import * as express from 'express';
+import catsRouter from './cats/cats.route';
 
-import catsRoute from './cats/cats.route'
+class Server {
+  public app: express.Application;
 
-const app: Express = express()
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
 
+  private setRoute() {
+    this.app.use(catsRouter);
+  }
 
-//* logging middleware
-app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log(req.rawHeaders[1]);
-  console.log('this is logging middleware');
-  next();
-});
+  private setMiddleware() {
+    //* logging middleware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log('this is logging middleware');
+      next();
+    });
 
-//* json middleware
-app.use(express.json());
+    //* json middleware
+    this.app.use(express.json());
 
-app.use(catsRoute);
+    this.setRoute();
 
-//* 404 middleware
-app.use((req, res, next) => {
-  console.log('this is error middleware');
-  res.send({error: '404 not found error'});
-});
+    //* 404 middleware
+    this.app.use((req, res, next) => {
+      console.log('this is error middleware');
+      res.send({error: '404 not found error'});
+    });
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(8000, () => {
+      console.log('server is on...');
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
